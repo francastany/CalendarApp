d52d33dcceae4b75c06d7726389b414a9d20c49b
Franco Castany <francocastany@gmail.com>
2022-08-17 19:20:21 -0300

    Agregando Contacts

--- js/main.js
+++ js/main.js
@@ -5,9 +5,11 @@ const fechaActual = new Date().toLocaleDateString(); //Fecha de hoy en formato S
 const agregarEvento = document.getElementById("agregarEventoForm")
 const eventosLista = document.getElementById("eventosLista")
 const proxEventosLista = document.getElementById("proxEventosLista")
+const contactoForm = document.getElementById("contactoForm")
+const contactosLista = document.getElementById("contactosLista")
 
 
-//Creamos la clase constructora para los objetos "Eventos"
+//Creamos la clase constructora para los objetos "Eventos" "Contactos"
 class Evento{
     constructor(name, description, date, timeStart, timeEnd) {
         this.name = name
@@ -17,9 +19,19 @@ class Evento{
         this.timeEnd = timeEnd
     }
 }
+//Creamos la clase constructora para los objetos "Contactos"
+class Contacto{
+    constructor(imagen, nombre, email, telefono) {
+        this.imagen = imagen
+        this.nombre = nombre
+        this.email = email
+        this.telefono = telefono
+    }
+}
 
-//Creamos el Array vacío donde guardaremos los Eventos
+//Creamos los Array vacíos donde guardaremos los Eventos y los Contactos
 let eventos = []
+let contactos = []
 
 //Definimos la función para mostrar los eventos en pantalla
 function mostrarEvento() {
@@ -79,20 +91,36 @@ function eliminarEvento() {
     }) 
 }
 
-//Llamamos al Local Storage; sino, lo creamos.
-(localStorage.getItem("eventos")) ? eventos = JSON.parse(localStorage.getItem("eventos")) : localStorage.setItem("eventos", JSON.stringify(eventos))
+function mostrarContactos() {
+    let contactosStorage = JSON.parse(localStorage.getItem("contactos"))
+    console.log(contactosStorage);
 
-/* Antigua forma de llamar al Local Storage 
-if(localStorage.getItem("eventos")) {
-   eventos = JSON.parse(localStorage.getItem("eventos"))
-} else { //si no existe una key "tareas", entonces lo crea
-    localStorage.setItem("eventos", JSON.stringify(eventos))
-} */
+    contactosLista.innerHTML = ""
+    contactosStorage.forEach((contacto, indice) => {
+        contactosLista.innerHTML += 
+        `
+        <div class="contacto bg-light" id="contacto${indice}">
+            <span>${contacto.nombre}</span>
+            <span>${contacto.email}</span>
+            <span>${contacto.telefono}</span>
+            <span>
+                <svg height="18px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.1.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M160 400C160 408.8 152.8 416 144 416C135.2 416 128 408.8 128 400V192C128 183.2 135.2 176 144 176C152.8 176 160 183.2 160 192V400zM240 400C240 408.8 232.8 416 224 416C215.2 416 208 408.8 208 400V192C208 183.2 215.2 176 224 176C232.8 176 240 183.2 240 192V400zM320 400C320 408.8 312.8 416 304 416C295.2 416 288 408.8 288 400V192C288 183.2 295.2 176 304 176C312.8 176 320 183.2 320 192V400zM317.5 24.94L354.2 80H424C437.3 80 448 90.75 448 104C448 117.3 437.3 128 424 128H416V432C416 476.2 380.2 512 336 512H112C67.82 512 32 476.2 32 432V128H24C10.75 128 0 117.3 0 104C0 90.75 10.75 80 24 80H93.82L130.5 24.94C140.9 9.357 158.4 0 177.1 0H270.9C289.6 0 307.1 9.358 317.5 24.94H317.5zM151.5 80H296.5L277.5 51.56C276 49.34 273.5 48 270.9 48H177.1C174.5 48 171.1 49.34 170.5 51.56L151.5 80zM80 432C80 449.7 94.33 464 112 464H336C353.7 464 368 449.7 368 432V128H80V432z"/></svg>
+            </span>
+        </div> 
+        `
+    })
 
-mostrarEvento();
-eliminarEvento();
+}
+
+//Llamamos a la key en el Local Storage; sino, la creamos.
+(localStorage.getItem("eventos")) ? eventos = JSON.parse(localStorage.getItem("eventos")) : localStorage.setItem("eventos", JSON.stringify(eventos));
+
+(localStorage.getItem("contactos")) ? contactos = JSON.parse(localStorage.getItem("contactos")) : localStorage.setItem("contactos", JSON.stringify(contactos))
+
 
 if (eventosLista != null || agregarEvento != null) {
+    mostrarEvento();
+    eliminarEvento();
     // Evento Submit del formulario para crear los eventos.
     agregarEvento.addEventListener("submit", (event) => {
         event.preventDefault() //Prevenimos recarga de la página
@@ -111,11 +139,9 @@ if (eventosLista != null || agregarEvento != null) {
         eventos.push(evento)
         // eventos.sort((evento1, evento2) => {evento1.date - evento2.date})
 
-        //Pisamos el Local Storage con el Array que contiene el evento creado
-        localStorage.setItem("eventos", JSON.stringify(eventos))
+        localStorage.setItem("eventos", JSON.stringify(eventos))//Pisamos el Local Storage con el Array que contiene el evento creado
         
         console.log(eventos)
-        // console.log(evento.date, fechaActual)
     
         agregarEvento.reset() //Reseteamos el Form
 
@@ -135,6 +161,36 @@ if (eventosLista != null || agregarEvento != null) {
     agregarEvento.addEventListener("submit", (evento) => {
         mostrarEvento();
         eliminarEvento();
+    })
+
+} else {
+    mostrarContactos();
+    
+    contactoForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let contactoNombre = document.getElementById("contactoNombre").value
+        let contactoEmail = document.getElementById("contactoEmail").value
+        let contactoTelefono = document.getElementById("contactoTelefono").value
+
+        const contacto = new Contacto (contactoNombre, contactoEmail, contactoTelefono)
+
+        contactos.push(contacto)
+
+        localStorage.setItem("contactos", JSON.stringify(contactos))
+
+        contactoForm.reset() //Reseteamos el Form
+
+        Toastify({ //Creamos toast para evento creado
+            text: "¡Contacto creado correctamente!",
+            duration: 2000,
+            gravity: "bottom", // `top` or `bottom`
+            position: "center", // `left`, `center` or `right`
+            stopOnFocus: false, // Prevents dismissing of toast on hover
+            style: {
+              background: "#FF8E3C",
+            },
+        }).showToast();
+
     })
 
 }
